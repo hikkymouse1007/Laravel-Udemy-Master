e0fe07705b32a7fd34534896bee4611f588bd74e
hikkymouse1007 <mc.mdt1007it@gmail.com>
2020-06-28 22:10:47 +0900

    finish section3-8

--- app/resources/views/blog-post.blade.php
+++ app/resources/views/blog-post.blade.php
@@ -1,5 +1,7 @@
 @extends('layout')
 
 @section('content')
-<h1>Welcome to my house!</h1>
+
+{{ $welcome }} {{ $data['title'] }}
+
 @endsection
--- app/routes/web.php
+++ app/routes/web.php
@@ -13,12 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::view('/', 'home');
 
 Route::get('/contact', function () {
-    return view('home');
+    return view('contact');
+});
+
+Route::view('/contact', 'contact');
+
+Route::get('/blog-post/{id}/{welcome?}', function ($id, $welcome = 1) {
+  $pages = [
+      1 => [
+          'title' => 'Hello from page1'
+      ],
+
+      2 => [
+          'title' => 'Hello from page2'
+      ],
+  ];
+
+  $welcomes = [
+      1 => 'Hello ',
+      2 => 'Welcome to'
+    ];
+
+  return view('blog-post', [
+      'data' => $pages[$id],
+      'welcome' => $welcomes[$welcome],
+      ]);
 });
 
 
